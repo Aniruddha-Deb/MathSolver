9e352cc50d91f0dc847076563c0a63ffed9b6248
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-07-09 23:40:16 +0530

    Changed configuration settings

--- mathsolver/api.py
+++ mathsolver/api.py
@@ -4,11 +4,10 @@ import io
 from mathsolver.ai import network
 from PIL import Image, ImageOps
 
-bp = Blueprint('api', __name__, url_prefix='/api')
+bp = Blueprint('api', __name__)
 
-@bp.route('/get_answer', methods=['POST'])
+@bp.route('/get_answer', methods=['POST', 'GET'])
 def get_answer():
-    print(request.form)
     img_bytes = base64.b64decode(request.form.get('img'))
     img_buf = io.BytesIO(img_bytes)
     img = ImageOps.grayscale(Image.open(img_buf))
